90f323f1ced640c1d54d5be276d4b4fa6dd3a502
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-10-31 16:36:46 +0400

    Hello World

---
+++ 001_Example/Example.java
@@ -0,0 +1,10 @@
+/*
+	Пример простой программы на Java.
+	Имя файла - Example.java
+*/
+
+class Example {
+	public static void main(String args[]) {
+		System.out.println("Java drives the Web.");
+	}
+}
