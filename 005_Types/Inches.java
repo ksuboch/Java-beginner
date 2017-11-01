2d8711e0de679168c20145abc2b539f956c83e6f
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-11-01 15:25:17 +0400

    Java types

---
+++ 005_Types/Inches.java
@@ -0,0 +1,15 @@
+/*
+    Расчет числа кубических дюймов в кубе объёмом в 1 милю
+*/
+class Inches {
+    public static void main(String[] args) {
+        long ci, im;
+
+        im = 5280 * 12;
+
+        ci = im * im * im;
+
+        System.out.println("There are " + ci +
+                           " cubic inches in cubic mile.");
+    }
+}
