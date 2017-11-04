60fcbc7e764a575a3164893db5b7292bb6fa97f0
ksuboch <3dvapper.inbox@gmail.com>
2017-11-04 12:34:50 +0400

    Array short initialisation

--- 005_Types/Array.java
+++ 005_Types/Array.java
@@ -1,7 +1,6 @@
 class Array {
     public static void main(String[] args) {
-        int month_days[];
-        month_days = new int[12];
+        int month_days[] = new int[12];
         month_days[0] = 31;
         month_days[1] = 28;
         month_days[2] = 31;
