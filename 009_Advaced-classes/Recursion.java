a8c4a5653a2f5e7c0ece274997813ab2fc5f66e7
ksuboch <3dvapper.inbox@gmail.com>
2017-11-05 20:24:45 +0400

    Recursion

---
+++ 009_Advaced-classes/Recursion.java
@@ -0,0 +1,19 @@
+class Factorial {
+    int fact(int n) {
+        int result;
+
+        if(n==1) return 1;
+        result = fact(n - 1) * n;
+        return result;
+    }
+}
+
+class Recursion {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+
+        System.out.println("Factorial of 3  = " + f.fact(3));
+        System.out.println("Factorial of 4  = " + f.fact(4));
+        System.out.println("Factorial of 5  = " + f.fact(5));
+    }
+}
