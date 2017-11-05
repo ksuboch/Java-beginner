95a5cf683e0c7074d617c3db848d3342e60e3b5b
ksuboch <3dvapper.inbox@gmail.com>
2017-11-05 19:31:55 +0400

    Object in params

--- 009_Advaced-classes/OverloadCons.java
+++ 009_Advaced-classes/OverloadCons.java
@@ -6,6 +6,12 @@ class Box {
         width = height = depth = -1;
     }
 
+    Box(Box b) {
+        width = b.width;
+        height = b.height;
+        depth = b.depth;
+    }
+
     Box(double l) {
         width = height = depth = l;
     }
@@ -26,9 +32,11 @@ class OverloadCons {
         Box mybox1 = new Box();
         Box mybox2 = new Box(7);
         Box mybox3 = new Box(10, 20, 15);
+        Box mybox4 = new Box(mybox3);
 
         System.out.println("MyBox1 volume = " + mybox1.volume());
         System.out.println("MyBox2 volume = " + mybox2.volume());
         System.out.println("MyBox3 volume = " + mybox3.volume());
+        System.out.println("MyBox4 volume = " + mybox4.volume());
     }
 }
