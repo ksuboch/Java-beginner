70155d396962b85a39ed48b042d61ecf933a6c88
ksuboch <3dvapper.inbox@gmail.com>
2017-11-13 22:27:37 +0400

    Overriding methods

---
+++ 010_Inheritance/Override.java
@@ -0,0 +1,45 @@
+class A {
+    int i, j;
+    A(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    void show() {
+        System.out.println("i and j = " + i + " " + j);
+    }
+
+    void shown() {
+        System.out.println("i and j = " + i + " " + j);
+    }
+
+    void show(String msg) {
+        System.out.println(msg + " " + i + " " + j);
+    }
+}
+
+class B extends A {
+    int k;
+    B(int a, int b, int c) {
+        super(a, b);
+        k = c;
+    }
+
+    void show() {
+        System.out.println("k = " + k);
+    }
+
+    void shown() {
+        super.shown();
+        System.out.println("k = " + k);
+    }
+}
+
+class Override {
+    public static void main(String[] args) {
+        B subOb = new B(1, 2, 3);
+        subOb.show();
+        subOb.shown();
+        subOb.show("Test sc method ");
+    }
+}
