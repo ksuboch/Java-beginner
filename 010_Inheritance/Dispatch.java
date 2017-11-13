e259c7b19ec0ea00058b19cadd0025083686d96d
ksuboch <3dvapper.inbox@gmail.com>
2017-11-13 22:44:54 +0400

    Dispatching methods

---
+++ 010_Inheritance/Dispatch.java
@@ -0,0 +1,36 @@
+class A {
+    void callme() {
+        System.out.println("Callme from A");
+    }
+}
+
+class B extends A {
+    void callme() {
+        System.out.println("Callme from B");
+    }
+}
+
+class C extends B {
+    void callme() {
+        System.out.println("Callme from C");
+    }
+}
+
+class Dispatch {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        C c = new C();
+
+        A r;
+
+        r = a;
+        r.callme();
+
+        r = b;
+        r.callme();
+
+        r = c;
+        r.callme();
+    }
+}
