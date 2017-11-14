bd13ed51ce52f50c307caa2f4d91b47d2017fb67
ksuboch <3dvapper.inbox@gmail.com>
2017-11-14 22:25:52 +0400

    Handling exception

---
+++ 012_Exceptions/HandleException.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+class HandleException {
+    public static void main(String[] args) {
+        int a = 0, b = 0, c = 0;
+        Random r = new Random();
+
+        for(int i = 0; i < 32000; i++) {
+            try {
+                b = r.nextInt();
+                c = r.nextInt();
+                a = 12345 / (b / c);
+            } catch (ArithmeticException e) {
+                System.out.println("Division by 0");
+                a = 0;
+            }
+            System.out.println("a : " + a);
+        }
+    }
+}
